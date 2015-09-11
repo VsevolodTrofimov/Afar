2c08864527433a29f585842def26804d2dc09a0f
VsevolodTrofimov <seva.trofimov2@gmail.com>
2015-09-11 20:43:15 +0300

    0.0.2
    Added player model, leg launch animations and animation controller.
    
    Created Skybox

---
+++ Models/Player/ForceTopLeg.js
@@ -0,0 +1,78 @@
+﻿#pragma strict
+
+var anim : Animator;
+var rb   : Rigidbody;
+
+public class Legs {
+	var left : boolean;
+	var right : boolean;
+	var top : boolean;
+	var bottom : boolean;
+	var back : boolean;
+	function ClassName(){
+		this.left = false;
+		this.right = false;
+		this.top = false;
+		this.bottom = false;
+		this.back = false;
+	     
+   }
+	
+};
+
+var legs = new Legs();
+
+function Start () {
+	anim = GetComponent("Animator");
+	rb = GetComponent.<Rigidbody>();
+};
+
+function LateUpdate () {
+	
+	anim.SetBool("LegLeftLaunch", legs.left);
+	anim.SetBool("LegRightLaunch", legs.right);
+	anim.SetBool("LegTopLaunch", legs.top);
+	anim.SetBool("LegBottomLaunch", legs.bottom);
+	anim.SetBool("LegBackLaunch", legs.back);
+		
+	if(Input.GetKeyDown(KeyCode.D)){
+		if(legs.left){
+			legs.left = false;
+		} else {
+			legs.left = true;
+		}
+	}
+	
+	if(Input.GetKeyDown(KeyCode.A)){
+		if(legs.right){
+			legs.right = false;
+		} else {
+			legs.right = true;
+		}
+	}
+	
+	if(Input.GetKeyDown(KeyCode.W)){
+		if(legs.top){
+			legs.top = false;
+		} else {
+			legs.top = true;
+		}
+	}
+	
+	if(Input.GetKeyDown(KeyCode.X)){
+		if(legs.bottom){
+			legs.bottom = false;
+		} else {
+			legs.bottom = true;
+		}
+	}
+	
+	if(Input.GetKeyDown(KeyCode.S)){
+		if(legs.back){
+			legs.back = false;
+		} else {
+			legs.back = true;
+			
+		}
+	}
+};
